7911e30ed4cffb955012a6ebe537b44299f7bc8c
fernandakaory <rm551104@fiap.com.br>
2023-03-07 11:50:00 -0300

    adicionamos src e pasta

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//este é um comentário de linha
+
+/**
+ * É um comentário 
+ * de
+ * bloco
+ */
+
+console.log("Olá mundo")
+console.error("Bom dia")
+console.warn("Será")
+console.info("Vai ser...")
